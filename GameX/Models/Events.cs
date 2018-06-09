de16c572ec06127933be22c4fc2f5f1d0b654848
Lukasz Basinski <basinski.luk@gmail.com>
2018-06-09 23:51:37 +0200

    Event table

---
+++ GameX/Models/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameX.Models
+{
+    public class Events
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public string Coords { get; set; }
+        
+    }
+}
