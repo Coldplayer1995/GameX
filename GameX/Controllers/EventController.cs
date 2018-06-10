5e6182beb10426004f42639e6892bd232188341c
Patryk Buda <coldplayer1995@gmail.com>
2018-06-10 19:03:34 +0100

    AddEvent first stage

---
+++ GameX/Controllers/EventController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameX.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameX.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly StoreContext _context;
+        public EventController(StoreContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public 
+    }
+}
