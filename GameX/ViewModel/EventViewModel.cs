108e5661cee46a47e6d131d11e05e25f8d87a7ad
Patryk Buda <coldplayer1995@gmail.com>
2018-06-10 21:17:16 +0100

    Stage2

---
+++ GameX/ViewModel/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GameX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameX.ViewModel
+{
+    public class EventViewModel
+    {
+        public List<Events> Events { get; set; }
+
+    }
+}
